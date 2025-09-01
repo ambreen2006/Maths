3c9e610fd73f9ce8a5dd12656ce05dae99213df5
Ambreen Haleem <ambreen2006@gmail.com>
2025-08-31 21:19:15 -0400

    Fix: labeling of radians on the unit circle

--- Trigonometry/sketch_sincosine.js
+++ Trigonometry/sketch_sincosine.js
@@ -107,29 +107,6 @@ for (let k = 0; k <= numTicks; k++) {
 }
 }
 
-// function animationButton(cnvX, cnvY) {
-//   button = createButton('⏸ Pause');
-
-//   button.style('padding', '10px 20px');
-//   button.style('font-size', '16px');
-//   button.style('border', 'none');
-//   button.style('border-radius', '8px');
-//   button.style('background-color', '#c104faff'); // green
-//   button.style('color', 'white');
-//   button.style('cursor', 'pointer');
-//   button.style('box-shadow', '2px 2px 6px rgba(0,0,0,0.3)');
-
-//    // hover effect
-//   button.mouseOver(() => button.style('background-color', '#b4049dff'));
-//   button.mouseOut(() => button.style('background-color', '#c104faff'));
-
-//   button.position(width-12, 100);
-//   button.mousePressed(() => {
-//     running = !running;
-//     button.html(running ? '⏸ Pause' : '▶ Start');
-//   });
-// }
-
 function setTitle() {
   fill(0);
   noStroke();
@@ -148,7 +125,7 @@ function drawUnitCircle() {
   pop();
 
   const angles = [0, Math.PI/4, Math.PI/2, 3*Math.PI/4, Math.PI, 5*Math.PI/4, 3*Math.PI/2, 7*Math.PI/4];
-  const angleLabels = ['2π','π/4', 'π/2', '3π/4', 'π', '3π/4', '3π/2', '7π/4'];
+  const angleLabels = ['2π','π/4', 'π/2', '3π/4', 'π', '5π/4', '3π/2', '7π/4'];
   angles.forEach((angle, index) => {
     const x = R * Math.cos(angle) * S;
     const y = R * Math.sin(angle) * S;
